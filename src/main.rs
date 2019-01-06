c8777bf1701aa8abf53e2f421a338b8650e9cd45
Matt Bilker <me@mbilker.us>
2019-01-06 00:27:04 -0500

    main: semi-working image pull

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use] extern crate lazy_static;
 
-use std::io::{self, Read, Write};
-use std::net::{Ipv4Addr, TcpStream};
+use std::io::{self, BufRead, BufReader, Read, Write};
+use std::net::{Ipv4Addr, Shutdown, TcpStream};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
@@ -9,7 +9,7 @@ use std::time::Duration;
 use crossbeam::atomic::ArcCell;
 use failure::Fallible;
 //use ffmpeg::software;
-use image::{GenericImageView, Pixel};
+use image::{FilterType, GenericImageView, ImageBuffer, Pixel};
 use rand::Rng;
 use rand::seq::SliceRandom;
 
@@ -20,7 +20,7 @@ use crate::socket::{cleanup_sockets, init_sockets, make_socket};
 const WIDTH: usize = 1024;
 const HEIGHT: usize = 768;
 
-const SPLIT_FACTOR: usize = 50;
+const SPLIT_FACTOR: usize = 900;
 
 lazy_static! {
     static ref CANVAS: ArcCell<Vec<Vec<u8>>> = {
@@ -30,14 +30,17 @@ lazy_static! {
 
 struct Pixelflut {
     stream: TcpStream,
+    //reader: BufReader<TcpStream>,
 }
 
 impl Pixelflut {
     fn new(local_addr: Ipv4Addr) -> Self {
         let remote_addr = Ipv4Addr::new(10, 13, 38, 233);
         let stream = make_socket(local_addr, remote_addr);
+        //let reader = BufReader::new(stream.try_clone().unwrap());
         Self {
             stream,
+            //reader,
         }
     }
 
@@ -56,6 +59,7 @@ impl Pixelflut {
     fn cmd_response(&mut self, buf: &mut [u8], cmd: &[u8]) -> std::io::Result<usize> {
         self.cmd(cmd)?;
 
+        //let len = self.reader.read_line(buf)?;
         let len = self.stream.read(buf)?;
         println!("len: {}", len);
 
@@ -130,7 +134,8 @@ fn static_bg() -> Vec<Vec<u8>> {
 fn image_bg() -> Vec<Vec<u8>> {
     let mut rng = rand::thread_rng();
 
-    let img = image::open("bg05.png").unwrap();
+    let img = image::open("kaiden.png").unwrap();
+    let img = img.resize_to_fill(WIDTH as u32, HEIGHT as u32, FilterType::CatmullRom);
 
     let (width, height) = img.dimensions();
     let buf = img.to_rgb();
@@ -160,14 +165,6 @@ fn image_bg() -> Vec<Vec<u8>> {
     cmd_buf
 }
 
-/*
-#[inline]
-fn video_bg() -> Vec<Vec<u8>> {
-    format::input("video.mp4").unwrap();
-    unimplemented!();
-}
-*/
-
 #[inline]
 fn canvas_thread() -> Fallible<()> {
     loop {
@@ -176,6 +173,8 @@ fn canvas_thread() -> Fallible<()> {
         //let cmd_buf = static_bg();
         let cmd_buf = image_bg();
         let chunk_size = cmd_buf.len() / SPLIT_FACTOR;
+
+        println!("commands: {}, chunk_size: {}", cmd_buf.len(), chunk_size);
 
         let mut thread_data: Vec<Vec<u8>> = Vec::with_capacity(SPLIT_FACTOR);
 
@@ -186,6 +185,16 @@ fn canvas_thread() -> Fallible<()> {
                 thread_data.push(part);
             }
         }
+
+        /*
+        for (i, thread) in thread_data.iter().enumerate() {
+            let num_commands = thread.iter()
+                .filter(|&ch| *ch == b'\n')
+                .count();
+
+            println!("thread_data[{}].len() = {}", i, num_commands);
+        }
+        */
 
         /*
         for (i, part) in cmd_buf.into_iter().enumerate() {
@@ -222,6 +231,73 @@ fn network_thread(i: usize, local_addr: Ipv4Addr) -> Fallible<()> {
     //Ok(())
 }
 
+fn pull_image() -> Fallible<()> {
+    let mut pf = Pixelflut::new(Ipv4Addr::UNSPECIFIED);
+    let mut buf = [0; 1024];
+
+    let len = pf.cmd_response(&mut buf, &b"SIZE\n"[..])?;
+    let value = std::str::from_utf8(&buf[..len - 1])?;
+    let values: Vec<_> = value.split(' ').collect();
+    //let values: Vec<_> = buf[..len - 1].split(' ').collect();
+
+    let width = values[1].parse::<usize>()?;
+    let height = values[2].parse::<usize>()?;
+    println!("width: {}, height: {}", width, height);
+
+    let mut canvas = ImageBuffer::new(width as u32, height as u32);
+    let mut cmd_buf = Vec::new();
+
+    for x in 0..width {
+        println!("establishing connection");
+        pf = Pixelflut::new(Ipv4Addr::UNSPECIFIED);
+        pf.stream.set_nodelay(true)?;
+        pf.stream.set_read_timeout(None)?;
+        pf.stream.set_write_timeout(None)?;
+
+        cmd_buf.clear();
+
+        for y in 0..height {
+            cmd_buf.write_fmt(format_args!("PX {} {}\n", x, y))?;
+        }
+
+        println!("writing to buf");
+        pf.cmd(&cmd_buf)?;
+        println!("flushing");
+        pf.stream.flush()?;
+
+        let mut reader = BufReader::new(&mut pf.stream);
+        let mut buf = String::new();
+
+        for _ in 0..height {
+            let len = reader.read_line(&mut buf)?;
+            println!("result: {} (len: {})", buf.trim(), len);
+
+            if len == 0 {
+                break;
+            }
+
+            // PX {x} {y} {color}
+            let mut values = buf.split(' ').skip(1);
+            let x = values.next().unwrap().parse::<u32>()?;
+            let y = values.next().unwrap().parse::<u32>()?;
+            let col = values.next().unwrap();
+            println!("col: {}", col.trim());
+
+            let r = u8::from_str_radix(&col[0..2], 16)?;
+            let g = u8::from_str_radix(&col[2..4], 16)?;
+            let b = u8::from_str_radix(&col[4..6], 16)?;
+            println!("(x, y) (r,g,b): ({},{}) ({},{},{})", x, y, r, g, b);
+            canvas[(x as u32, y as u32)] = image::Rgb([r, g, b]);
+
+            buf.clear();
+        }
+    }
+
+    canvas.save("pixelflut.png").unwrap();
+
+    Err(std::io::Error::last_os_error().into())
+}
+
 fn main() {
     let addrs = &[
         Ipv4Addr::new(10, 13, 38, 159),
@@ -230,6 +306,8 @@ fn main() {
     let mut handles = Vec::with_capacity(8);
 
     init_sockets();
+
+    pull_image().unwrap();
 
     handles.push(thread::spawn(move || {
         canvas_thread().unwrap();
